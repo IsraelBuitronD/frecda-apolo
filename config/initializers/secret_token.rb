47e87e4f73b1aa05dd0da21e9eaefc799567afcb
Israel Buitron <israel.buitron@gmail.com>
2013-04-06 21:55:22 -0600

    Initial commit.
    Nothing done, only app generation. :)
    In memory of Apolo, Jose Carranza friend.
    
    Apolo's photo: https://fbcdn-sphotos-h-a.akamaihd.net/hphotos-ak-prn1/31447_3823215478818_914624289_n.jpg
    
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apolo::Application.config.secret_token = 'e4851a15ee1b6c511106c98e1db2b6629c2323790b1a10ed2e929ba228379b72d4d6164256c086cc0bcbdc70dfdb08e916f411755f1620abd9654a4966223293'

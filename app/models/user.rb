b649a1cb886023c7d3170bcea227dcc34972b8ab
Israel Buitron <israel.buitron@gmail.com>
2013-06-10 14:26:15 -0500

    Method to create user using facebook authentication was added.
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :facebook_uid, :provider
+
+  def self.create_with_omniauth(auth)
+    create! do |user|
+      user.provider = auth["provider"]
+      user.facebook_uid = auth["uid"]
+    end
+  end
+
 end

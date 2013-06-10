c4d3c9f7f09073919fd2a3d16b5d850479a8a8ab
Israel Buitron <israel.buitron@gmail.com>
2013-06-10 13:41:44 -0500

    `User ` model was generated.
    Notice:
    - Do not forget to run `rake db:migrate`.
    
    Signed-off-by: Israel Buitron <israel.buitron@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130407040314) do
+ActiveRecord::Schema.define(:version => 20130610175844) do
 
   create_table "cases", :force => true do |t|
     t.string   "title"
@@ -25,6 +25,13 @@ ActiveRecord::Schema.define(:version => 20130407040314) do
     t.integer  "case_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "facebook_uid"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
